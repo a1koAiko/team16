d8ee077b2f7cd0e826a73132ff3fa22b9230ca6b
Morgan Johnson <maelstrom@redstonelab.net>
2017-09-30 11:34:15 -0600

    Third commit

---
+++ src/esof322/pa1/Vector3D.java
@@ -0,0 +1,10 @@
+package esof322.pa1;
+
+public class Vector3D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/esof322/pa1/Vector3DTest.java
@@ -0,0 +1,14 @@
+package esof322.pa1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Vector3DTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
