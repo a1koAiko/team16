2f6706dbc3627b77f3aadbdef1416b9a735cc9c5
Morgan Johnson <maelstrom@redstonelab.net>
2017-09-30 11:49:39 -0600

    Fourth update

--- test/esof322/pa1/Vector3DTest.java
+++ test/esof322/pa1/Vector3DTest.java
@@ -7,7 +7,44 @@ import org.junit.Test;
 public class Vector3DTest {
 
 	@Test
-	public void test() {
+	public void testScale() { // 
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd() { 
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSubtract() {
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testNegate() {
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDotProduct() {
+		Vector3D testInstance = new Vector3D();
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMagnitude() {
+		Vector3D testInstance = new Vector3D();
+		
 		fail("Not yet implemented");
 	}
 
